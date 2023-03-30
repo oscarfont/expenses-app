f9064ca2d8ba94d0726280a93bdb1f5ea1f87d7e
ofont <o.font@alumnos.upm.es>
2023-03-31 00:14:55 +0200

    added icons as components

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,11 @@
 <script>
-	import { homeIcon } from "./Icons";
+	import FileIcon from "./icons/FileIcon.svelte";
+    import HomeIcon from "./icons/HomeIcon.svelte";
+	import UserIcon from "./icons/UserIcon.svelte";
 </script>
 
 <div class="w-full bg-black">
-    {@html homeIcon}
+    <HomeIcon color={'var(--emerald)'}/>
+    <FileIcon color={'var(--emerald)'}/>
+    <UserIcon color={'var(--emerald)'}/>
 </div>
