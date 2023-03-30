4c53bb9e7f1efaf3111b51ec269f6622e7fd6fa0
ofont <o.font@alumnos.upm.es>
2023-03-30 22:17:01 +0200

    added header with icon

---
+++ src/components/Header.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { homeIcon } from "./Icons";
+</script>
+
+<div class="w-full bg-black">
+    {@html homeIcon}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Header from "../components/Header.svelte";
+</script>
+
 <main class="h-screen w-full flex justify-center">
-    <section class="h-screen w-full max-w-lg border-solid border-2 border-red-500"></section>
+    <section class="h-screen w-full max-w-lg border-solid border-2 border-red-500">
+        <Header />
+    </section>
 </main>
