7488f2559cec63f4559c05d66998fd99569d8d55
ofont <o.font@alumnos.upm.es>
2023-03-30 20:54:19 +0200

    added main layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1 class="text-3xl underline">
-    Hello world!
-</h1>
+<main class="h-screen w-full flex justify-center">
+    <section class="h-screen w-full max-w-lg border-solid border-2 border-red-500"></section>
+</main>
