076fa79fe12fada27e2751bb20a4e85c9d7614cd
ofont <o.font@alumnos.upm.es>
2023-03-31 17:06:31 +0200

    added some style to header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,20 +4,33 @@
 	import UserIcon from "./icons/UserIcon.svelte";
 </script>
 
-<footer class="w-full bg-tom-thumb grid grid-rows-1 grid-cols-3">
-    <a href="/">
-        <HomeIcon color={'var(--gray-nurse)'}/>
-    </a>
-    <a href="/history">
-        <FileIcon color={'var(--gray-nurse)'}/>
-    </a>
-    <button>
-        <UserIcon color={'var(--gray-nurse)'}/>
-    </button>
+<footer class="w-full bg-tom-thumb flex flex-col green-shadow">
+    <span class="w-full h-3 bg-transparent border-gray"></span>
+    <span class="w-full bg-tom-thumb grid grid-rows-1 grid-cols-3">
+        <a href="/">
+            <HomeIcon color={'var(--gray-nurse)'} className={'w-16 h-16 p-2'}/>
+        </a>
+        <a href="/history">
+            <FileIcon color={'var(--gray-nurse)'} className={'w-16 h-16 p-2'}/>
+        </a>
+        <button>
+            <UserIcon color={'var(--gray-nurse)'} className={'w-16 h-16 p-2'}/>
+        </button>
+    </span>
 </footer>
 
 <style>
     .bg-tom-thumb{
         background-color: var(--tom-thumb);
+    }
+    
+    .border-gray{
+        border-bottom: 4px solid var(--gray-nurse)
+    }
+
+    .green-shadow{
+        -webkit-box-shadow: -1px 0px 5px 0px var(--tom-thumb);
+        -moz-box-shadow: -1px 0px 5px 0px var(--tom-thumb);
+        box-shadow: -1px 0px 5px 0px var(--tom-thumb);
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main class="bg-gray-nurse h-screen w-full flex justify-center">
-    <section class="h-screen w-full max-w-lg border-solid border-2 border-red-500">
+    <section class="h-screen w-full max-w-lg flex flex-col justify-end border-solid border-2 border-red-500">
         <Header />
     </section>
 </main>
