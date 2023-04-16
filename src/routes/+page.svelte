0e9d499209cf54485d059e4d502d4e999aede802
ofont <o.font@alumnos.upm.es>
2023-04-16 20:01:51 +0200

    input styled

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Total from "../components/Total.svelte";
+	import SendIcon from "../components/icons/SendIcon.svelte";
 
     export let data;
 </script>
@@ -8,21 +9,23 @@
     <header class="w-full max-h-1/4 pt-2 grid grid-cols-12 font-firasans text-4xl text-emerald-light bg-pewter-dark shadow-md">
         <span class="flex items-center justify-center col-span-10 md:col-span-8 border-t-4 border-emerald-light ">Siguiente compra</span> 
         <span class="col-span-2 p-1 md:col-span-4 border-t-4 border-emerald-light ">
-            <img class="w-24 rounded-full p-2" src={`/${data?.defaulter}.png`} alt="avatar of defaulter"/>
+            <img class="w-24 rounded-sm p-2" src={`/${data?.defaulter}.png`} alt="avatar of defaulter"/>
         </span>
     </header>
     <body class="max-h-1/2">
         <form class="flex flex-col" method="POST">
             <div class="w-full flex justify-center gap-2 font-maitree text-2xl text-tom-thumb">
                 <span class="py-8 flex items-center">¡Hola, </span>
-                <span class="py-8 flex items-center gap-2"><img class="w-16 rounded-full" src={`/ofontito.png`} alt="avatar of user"/>!</span>
+                <span class="py-8 flex items-center gap-2"><img class="w-16 rounded-sm" src={`/ofontito.png`} alt="avatar of user"/>!</span>
                 <span class="flex items-center">¿Cuanto te has gastado?</span>
             </div>
-            <div class="flex">
-                <label class="flex flex-col">
-                  <input name="gasto" id="gasto" type="number" step=".01">
+            <div class="w-full flex gap-4 justify-center px-8">
+                <label class="w-full flex flex-col">
+                  <input name="gasto" id="gasto" type="number" class="bg-pewter-dark p-4 font-sintony text-tom-thumb rounded-sm" step=".01">
                 </label>
-                <button type="submit">SEND</button>
+                <button type="submit" class="bg-tom-thumb rounded-sm">
+                    <SendIcon color={'var(--emerald)'} className={'w-12 h-12 p-2'}/>
+                </button>
             </div> 
         </form>
         <section class="flex flex-col">
