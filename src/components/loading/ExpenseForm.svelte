d8a1bc7648680dcfe454065c033dc7272230737e
ofont <o.font@alumnos.upm.es>
2023-05-26 01:08:09 +0200

    fixed last loadings

--- src/components/loading/ExpenseForm.svelte
+++ src/components/loading/ExpenseForm.svelte
@@ -1,4 +1,5 @@
-<script>
-	import Card from "../Card.svelte";
-</script>
-<Card className={"mt-4 mb-16 animate-pulse flex-grow"}></Card>
+<div class="flex flex-col mt-4 mb-16 animate-pulse flex-grow gap-8">
+    <span class="w-full rounded-sm bg-main-green h-12 animate-pulse"></span>
+    <span class="w-full rounded-sm bg-main-green h-12 animate-pulse"></span>
+    <span class="w-full rounded-sm bg-main-green h-12 animate-pulse"></span>
+</div>
